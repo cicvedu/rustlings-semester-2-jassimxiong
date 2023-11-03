53d344f1f0712811f5b488af29c0a72e7531c363
熊义 <JassimXiong@gmail.com>
2023-11-04 01:41:59 +0800

    50% OK

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if 0 <= num  && num <= 40 {
+        2 * num
+    } else if num > 40 {
+        num
+    } else {
+        0
+    }
+}
 
 // Don't modify this function!
 #[test]
